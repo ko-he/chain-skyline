fa0ab822e9486d548be3f355b5584e0c888dac8f
ko-he <localhost.ko@gmail.com>
2020-04-13 20:29:50 +0900

    initialize application create chaincode from template

---
+++ contract/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const BoardingPassContract = require('./lib/boarding-pass-contract');
+
+module.exports.BoardingPassContract = BoardingPassContract;
+module.exports.contracts = [ BoardingPassContract ];
