bb5780e239d6c09429bbe6a62d28cab78fa32849
ko-he <localhost.ko@gmail.com>
2020-04-13 20:34:19 +0900

    add editor confing and fix indent

--- contract/lib/boarding-pass-contract.js
+++ contract/lib/boarding-pass-contract.js
@@ -8,48 +8,48 @@ const { Contract } = require('fabric-contract-api');
 
 class BoardingPassContract extends Contract {
 
-    async boardingPassExists(ctx, boardingPassId) {
-        const buffer = await ctx.stub.getState(boardingPassId);
-        return (!!buffer && buffer.length > 0);
-    }
+  async boardingPassExists(ctx, boardingPassId) {
+    const buffer = await ctx.stub.getState(boardingPassId);
+    return (!!buffer && buffer.length > 0);
+  }
 
-    async createBoardingPass(ctx, boardingPassId, value) {
-        const exists = await this.boardingPassExists(ctx, boardingPassId);
-        if (exists) {
-            throw new Error(`The boarding pass ${boardingPassId} already exists`);
-        }
-        const asset = { value };
-        const buffer = Buffer.from(JSON.stringify(asset));
-        await ctx.stub.putState(boardingPassId, buffer);
+  async createBoardingPass(ctx, boardingPassId, value) {
+    const exists = await this.boardingPassExists(ctx, boardingPassId);
+    if (exists) {
+      throw new Error(`The boarding pass ${boardingPassId} already exists`);
     }
+    const asset = { value };
+    const buffer = Buffer.from(JSON.stringify(asset));
+    await ctx.stub.putState(boardingPassId, buffer);
+  }
 
-    async readBoardingPass(ctx, boardingPassId) {
-        const exists = await this.boardingPassExists(ctx, boardingPassId);
-        if (!exists) {
-            throw new Error(`The boarding pass ${boardingPassId} does not exist`);
-        }
-        const buffer = await ctx.stub.getState(boardingPassId);
-        const asset = JSON.parse(buffer.toString());
-        return asset;
+  async readBoardingPass(ctx, boardingPassId) {
+    const exists = await this.boardingPassExists(ctx, boardingPassId);
+    if (!exists) {
+      throw new Error(`The boarding pass ${boardingPassId} does not exist`);
     }
+    const buffer = await ctx.stub.getState(boardingPassId);
+    const asset = JSON.parse(buffer.toString());
+    return asset;
+  }
 
-    async updateBoardingPass(ctx, boardingPassId, newValue) {
-        const exists = await this.boardingPassExists(ctx, boardingPassId);
-        if (!exists) {
-            throw new Error(`The boarding pass ${boardingPassId} does not exist`);
-        }
-        const asset = { value: newValue };
-        const buffer = Buffer.from(JSON.stringify(asset));
-        await ctx.stub.putState(boardingPassId, buffer);
+  async updateBoardingPass(ctx, boardingPassId, newValue) {
+    const exists = await this.boardingPassExists(ctx, boardingPassId);
+    if (!exists) {
+      throw new Error(`The boarding pass ${boardingPassId} does not exist`);
     }
+    const asset = { value: newValue };
+    const buffer = Buffer.from(JSON.stringify(asset));
+    await ctx.stub.putState(boardingPassId, buffer);
+  }
 
-    async deleteBoardingPass(ctx, boardingPassId) {
-        const exists = await this.boardingPassExists(ctx, boardingPassId);
-        if (!exists) {
-            throw new Error(`The boarding pass ${boardingPassId} does not exist`);
-        }
-        await ctx.stub.deleteState(boardingPassId);
+  async deleteBoardingPass(ctx, boardingPassId) {
+    const exists = await this.boardingPassExists(ctx, boardingPassId);
+    if (!exists) {
+      throw new Error(`The boarding pass ${boardingPassId} does not exist`);
     }
+    await ctx.stub.deleteState(boardingPassId);
+  }
 
 }
 
--- contract/test/boarding-pass-contract.js
+++ contract/test/boarding-pass-contract.js
@@ -19,90 +19,90 @@ chai.use(sinonChai);
 
 class TestContext {
 
-    constructor() {
-        this.stub = sinon.createStubInstance(ChaincodeStub);
-        this.clientIdentity = sinon.createStubInstance(ClientIdentity);
-        this.logging = {
-            getLogger: sinon.stub().returns(sinon.createStubInstance(winston.createLogger().constructor)),
-            setLevel: sinon.stub(),
-        };
-    }
+  constructor() {
+    this.stub = sinon.createStubInstance(ChaincodeStub);
+    this.clientIdentity = sinon.createStubInstance(ClientIdentity);
+    this.logging = {
+      getLogger: sinon.stub().returns(sinon.createStubInstance(winston.createLogger().constructor)),
+      setLevel: sinon.stub(),
+    };
+  }
 
 }
 
 describe('BoardingPassContract', () => {
 
-    let contract;
-    let ctx;
+  let contract;
+  let ctx;
 
-    beforeEach(() => {
-        contract = new BoardingPassContract();
-        ctx = new TestContext();
-        ctx.stub.getState.withArgs('1001').resolves(Buffer.from('{"value":"boarding pass 1001 value"}'));
-        ctx.stub.getState.withArgs('1002').resolves(Buffer.from('{"value":"boarding pass 1002 value"}'));
+  beforeEach(() => {
+    contract = new BoardingPassContract();
+    ctx = new TestContext();
+    ctx.stub.getState.withArgs('1001').resolves(Buffer.from('{"value":"boarding pass 1001 value"}'));
+    ctx.stub.getState.withArgs('1002').resolves(Buffer.from('{"value":"boarding pass 1002 value"}'));
+  });
+
+  describe('#boardingPassExists', () => {
+
+    it('should return true for a boarding pass', async () => {
+      await contract.boardingPassExists(ctx, '1001').should.eventually.be.true;
     });
 
-    describe('#boardingPassExists', () => {
-
-        it('should return true for a boarding pass', async () => {
-            await contract.boardingPassExists(ctx, '1001').should.eventually.be.true;
-        });
-
-        it('should return false for a boarding pass that does not exist', async () => {
-            await contract.boardingPassExists(ctx, '1003').should.eventually.be.false;
-        });
-
+    it('should return false for a boarding pass that does not exist', async () => {
+      await contract.boardingPassExists(ctx, '1003').should.eventually.be.false;
     });
 
-    describe('#createBoardingPass', () => {
+  });
 
-        it('should create a boarding pass', async () => {
-            await contract.createBoardingPass(ctx, '1003', 'boarding pass 1003 value');
-            ctx.stub.putState.should.have.been.calledOnceWithExactly('1003', Buffer.from('{"value":"boarding pass 1003 value"}'));
-        });
+  describe('#createBoardingPass', () => {
 
-        it('should throw an error for a boarding pass that already exists', async () => {
-            await contract.createBoardingPass(ctx, '1001', 'myvalue').should.be.rejectedWith(/The boarding pass 1001 already exists/);
-        });
-
+    it('should create a boarding pass', async () => {
+      await contract.createBoardingPass(ctx, '1003', 'boarding pass 1003 value');
+      ctx.stub.putState.should.have.been.calledOnceWithExactly('1003', Buffer.from('{"value":"boarding pass 1003 value"}'));
     });
 
-    describe('#readBoardingPass', () => {
-
-        it('should return a boarding pass', async () => {
-            await contract.readBoardingPass(ctx, '1001').should.eventually.deep.equal({ value: 'boarding pass 1001 value' });
-        });
-
-        it('should throw an error for a boarding pass that does not exist', async () => {
-            await contract.readBoardingPass(ctx, '1003').should.be.rejectedWith(/The boarding pass 1003 does not exist/);
-        });
-
+    it('should throw an error for a boarding pass that already exists', async () => {
+      await contract.createBoardingPass(ctx, '1001', 'myvalue').should.be.rejectedWith(/The boarding pass 1001 already exists/);
     });
 
-    describe('#updateBoardingPass', () => {
+  });
 
-        it('should update a boarding pass', async () => {
-            await contract.updateBoardingPass(ctx, '1001', 'boarding pass 1001 new value');
-            ctx.stub.putState.should.have.been.calledOnceWithExactly('1001', Buffer.from('{"value":"boarding pass 1001 new value"}'));
-        });
+  describe('#readBoardingPass', () => {
 
-        it('should throw an error for a boarding pass that does not exist', async () => {
-            await contract.updateBoardingPass(ctx, '1003', 'boarding pass 1003 new value').should.be.rejectedWith(/The boarding pass 1003 does not exist/);
-        });
-
+    it('should return a boarding pass', async () => {
+      await contract.readBoardingPass(ctx, '1001').should.eventually.deep.equal({ value: 'boarding pass 1001 value' });
     });
 
-    describe('#deleteBoardingPass', () => {
-
-        it('should delete a boarding pass', async () => {
-            await contract.deleteBoardingPass(ctx, '1001');
-            ctx.stub.deleteState.should.have.been.calledOnceWithExactly('1001');
-        });
-
-        it('should throw an error for a boarding pass that does not exist', async () => {
-            await contract.deleteBoardingPass(ctx, '1003').should.be.rejectedWith(/The boarding pass 1003 does not exist/);
-        });
-
+    it('should throw an error for a boarding pass that does not exist', async () => {
+      await contract.readBoardingPass(ctx, '1003').should.be.rejectedWith(/The boarding pass 1003 does not exist/);
     });
+
+  });
+
+  describe('#updateBoardingPass', () => {
+
+    it('should update a boarding pass', async () => {
+      await contract.updateBoardingPass(ctx, '1001', 'boarding pass 1001 new value');
+      ctx.stub.putState.should.have.been.calledOnceWithExactly('1001', Buffer.from('{"value":"boarding pass 1001 new value"}'));
+    });
+
+    it('should throw an error for a boarding pass that does not exist', async () => {
+      await contract.updateBoardingPass(ctx, '1003', 'boarding pass 1003 new value').should.be.rejectedWith(/The boarding pass 1003 does not exist/);
+    });
+
+  });
+
+  describe('#deleteBoardingPass', () => {
+
+    it('should delete a boarding pass', async () => {
+      await contract.deleteBoardingPass(ctx, '1001');
+      ctx.stub.deleteState.should.have.been.calledOnceWithExactly('1001');
+    });
+
+    it('should throw an error for a boarding pass that does not exist', async () => {
+      await contract.deleteBoardingPass(ctx, '1003').should.be.rejectedWith(/The boarding pass 1003 does not exist/);
+    });
+
+  });
 
 });
