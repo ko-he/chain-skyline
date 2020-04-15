88b04f5de9a634cc3ded4d47f24cd0760a89743b
ko-he <localhost.ko@gmail.com>
2020-04-15 16:31:47 +0900

    change indent size

--- contacts/passenger/lib/passenger-contract.js
+++ contacts/passenger/lib/passenger-contract.js
@@ -8,48 +8,48 @@ const { Contract } = require('fabric-contract-api');
 
 class PassengerContract extends Contract {
 
-    async passengerExists(ctx, passengerId) {
-        const buffer = await ctx.stub.getState(passengerId);
-        return (!!buffer && buffer.length > 0);
-    }
+  async passengerExists(ctx, passengerId) {
+    const buffer = await ctx.stub.getState(passengerId);
+    return (!!buffer && buffer.length > 0);
+  }
 
-    async createPassenger(ctx, passengerId, value) {
-        const exists = await this.passengerExists(ctx, passengerId);
-        if (exists) {
-            throw new Error(`The passenger ${passengerId} already exists`);
-        }
-        const asset = { value };
-        const buffer = Buffer.from(JSON.stringify(asset));
-        await ctx.stub.putState(passengerId, buffer);
+  async createPassenger(ctx, passengerId, value) {
+    const exists = await this.passengerExists(ctx, passengerId);
+    if (exists) {
+      throw new Error(`The passenger ${passengerId} already exists`);
     }
+    const asset = { value };
+    const buffer = Buffer.from(JSON.stringify(asset));
+    await ctx.stub.putState(passengerId, buffer);
+  }
 
-    async readPassenger(ctx, passengerId) {
-        const exists = await this.passengerExists(ctx, passengerId);
-        if (!exists) {
-            throw new Error(`The passenger ${passengerId} does not exist`);
-        }
-        const buffer = await ctx.stub.getState(passengerId);
-        const asset = JSON.parse(buffer.toString());
-        return asset;
+  async readPassenger(ctx, passengerId) {
+    const exists = await this.passengerExists(ctx, passengerId);
+    if (!exists) {
+      throw new Error(`The passenger ${passengerId} does not exist`);
     }
+    const buffer = await ctx.stub.getState(passengerId);
+    const asset = JSON.parse(buffer.toString());
+    return asset;
+  }
 
-    async updatePassenger(ctx, passengerId, newValue) {
-        const exists = await this.passengerExists(ctx, passengerId);
-        if (!exists) {
-            throw new Error(`The passenger ${passengerId} does not exist`);
-        }
-        const asset = { value: newValue };
-        const buffer = Buffer.from(JSON.stringify(asset));
-        await ctx.stub.putState(passengerId, buffer);
+  async updatePassenger(ctx, passengerId, newValue) {
+    const exists = await this.passengerExists(ctx, passengerId);
+    if (!exists) {
+      throw new Error(`The passenger ${passengerId} does not exist`);
     }
+    const asset = { value: newValue };
+    const buffer = Buffer.from(JSON.stringify(asset));
+    await ctx.stub.putState(passengerId, buffer);
+  }
 
-    async deletePassenger(ctx, passengerId) {
-        const exists = await this.passengerExists(ctx, passengerId);
-        if (!exists) {
-            throw new Error(`The passenger ${passengerId} does not exist`);
-        }
-        await ctx.stub.deleteState(passengerId);
+  async deletePassenger(ctx, passengerId) {
+    const exists = await this.passengerExists(ctx, passengerId);
+    if (!exists) {
+      throw new Error(`The passenger ${passengerId} does not exist`);
     }
+    await ctx.stub.deleteState(passengerId);
+  }
 
 }
 
