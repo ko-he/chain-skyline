63a80c45b7ec5624b94fbb48c1ccc068f860f973
ko-he <localhost.ko@gmail.com>
2020-04-15 16:21:05 +0900

    add chain code

---
+++ contacts/passenger/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const PassengerContract = require('./lib/passenger-contract');
+
+module.exports.PassengerContract = PassengerContract;
+module.exports.contracts = [ PassengerContract ];
