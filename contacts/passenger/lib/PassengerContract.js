b68acf09238493c23137f0a2ed1bf2f62015021b
ko-he <localhost.ko@gmail.com>
2020-04-16 17:16:03 +0900

    update contact

--- contacts/passenger/lib/PassengerContract.js
+++ contacts/passenger/lib/PassengerContract.js
@@ -48,6 +48,55 @@ class PassengerContract extends Contract {
     await ctx.stub.deleteState(passengerId);
   }
 
+  async queryAll(ctx) {
+
+    let queryString = {
+      selector: {}
+    };
+
+    let queryResults = await this.queryWithQueryString(ctx, JSON.stringify(queryString));
+    return queryResults;
+
+  }
+
+  async queryWithQueryString(ctx, queryString) {
+
+    console.log('query String');
+    console.log(JSON.stringify(queryString));
+
+    let resultsIterator = await ctx.stub.getQueryResult(queryString);
+
+    let allResults = [];
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      let res = await resultsIterator.next();
+
+      if (res.value && res.value.value.toString()) {
+        let jsonRes = {};
+
+        console.log(res.value.value.toString('utf8'));
+
+        jsonRes.Key = res.value.key;
+
+        try {
+          jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+        } catch (err) {
+          console.log(err);
+          jsonRes.Record = res.value.value.toString('utf8');
+        }
+
+        allResults.push(jsonRes);
+      }
+      if (res.done) {
+        console.log('end of data');
+        await resultsIterator.close();
+        console.info(allResults);
+        console.log(JSON.stringify(allResults));
+        return JSON.stringify(allResults);
+      }
+    }
+  }
 }
 
 module.exports = PassengerContract;
