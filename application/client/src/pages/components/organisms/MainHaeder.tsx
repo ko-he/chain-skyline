d8da19feeed96ee2565e88bd5359a014d1b6b05f
ko-he <localhost.ko@gmail.com>
2020-04-18 18:10:19 +0900

    change header style

--- application/client/src/pages/components/organisms/MainHaeder.tsx
+++ application/client/src/pages/components/organisms/MainHaeder.tsx
@@ -4,14 +4,13 @@ import { Layout, Menu } from 'antd'
 import styled from 'styled-components'
 
 const HeaderLogo = styled.div`
-  float: left;
   width: 200px;
   color: rgba(255, 255, 255, 0.85);
 `
 
 const MainHader: React.FC<{}> = _props => {
   return (
-    <Layout.Header className="header">
+    <Layout.Header className="header" style={{ display: 'flex', justifyContent: 'space-between' }}>
       <HeaderLogo>ChainSkeyline</HeaderLogo>
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
         <Menu.Item key="1">nav 1</Menu.Item>
