3f7d37108cd90f0cf78535e9f4ea402f887ac2bd
ko-he <localhost.ko@gmail.com>
2020-04-18 14:14:37 +0900

    create main haeder

--- application/client/src/pages/TopPage.tsx
+++ application/client/src/pages/TopPage.tsx
@@ -1,8 +1,17 @@
-import * as React from 'react'
+import React from 'react'
+import { Layout } from 'antd'
+import MainHader from './components/organisms/MainHaeder'
 
 const TopPage: React.FC<{}> = _props => {
   return (
-    <h1>TopPage</h1>
+    <Layout>
+      <MainHader />
+      <Layout.Content>
+        <Layout className="site-layout-background">
+          <Layout.Content style={{ minHeight: 280 }}>Content</Layout.Content>
+        </Layout>
+      </Layout.Content>
+    </Layout>
   )
 }
 
