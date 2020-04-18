0512c3d35552cff315be2ef3c488b4773d24038f
ko-he <localhost.ko@gmail.com>
2020-04-18 11:16:59 +0900

    build & serve

--- application/server/src/index.ts
+++ application/server/src/index.ts
@@ -7,7 +7,7 @@ const router = express.Router()
 router.get(
   '/',
   async (req: express.Request, res: express.Response): Promise<void> => {
-    res.send('hello world!!!')
+    res.send('hello world')
   }
 );
 
