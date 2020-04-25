7be49cdd28df9b1899c645ee3b1b3afa44602284
ko-he <localhost.ko@gmail.com>
2020-04-25 16:18:24 +0900

    create signUp form

--- application/client/src/pages/components/organisms/TopPageContent.tsx
+++ application/client/src/pages/components/organisms/TopPageContent.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout, Card, Form, Input, Checkbox, Button } from 'antd'
+import { EditOutlined, SettingOutlined } from '@ant-design/icons'
 import styled from 'styled-components'
 
 const TopImgWrapper = styled.div`
@@ -9,53 +10,104 @@ const TopImgWrapper = styled.div`
   background: center center no-repeat url("top_img.jpg");
   background-size:cover;
 `
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+}
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+}
 
 const TopPageContent: React.FC<{}> = _props => {
-  const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
-  }
-  const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
-  }
-
+  const [formType, setFormType] = React.useState<string>('SingIn')
   return(
     <Layout.Content>
       <TopImgWrapper>
-        <Card title="SingIn" extra="ChainSkyline" style={{ width: '400px', opacity: 0.7 }}>
-          <Form
-            {...layout}
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={() => {}}
-            onFinishFailed={() => {}}
-          >
-            <Form.Item
-              label="Username"
-              name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-              <Input />
-            </Form.Item>
+        <Card title={formType}
+          actions={[
+            <SettingOutlined key="setting" onClick={() => setFormType('SingIn')} />,
+            <EditOutlined key="edit" onClick={() => setFormType('SingUp')} />
+          ]}
+          extra="ChainSkyline"
+          style={{ width: '400px', opacity: 0.85 }}
+        >
+          {
+            formType === 'SingIn'
+              ? <Form
+                  {...layout}
+                  name="basic"
+                  initialValues={{ remember: true }}
+                  onFinish={() => {}}
+                  onFinishFailed={() => {}}
+                >
+                  <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                  >
+                    <Input />
+                  </Form.Item>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-              <Input.Password />
-            </Form.Item>
+                  <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                  >
+                    <Input.Password />
+                  </Form.Item>
 
-            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
+                  <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                    <Checkbox>Remember me</Checkbox>
+                  </Form.Item>
 
-            <Form.Item {...tailLayout}>
-              <Button type="primary" htmlType="submit">
-                Submit
-            </Button>
-            </Form.Item>
-          </Form>
+                  <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                      Submit
+                  </Button>
+                  </Form.Item>
+                </Form>
+              : <Form
+                  {...layout}
+                  name="basic"
+                  initialValues={{ remember: true }}
+                  onFinish={() => {}}
+                  onFinishFailed={() => {}}
+                >
+                  <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                  >
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your email!' }]}
+                  >
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                  >
+                    <Input.Password />
+                  </Form.Item>
+
+                  <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                    <Checkbox>Remember me</Checkbox>
+                  </Form.Item>
+
+                  <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                      Submit
+                  </Button>
+                  </Form.Item>
+                </Form>
+          }
         </Card>
       </TopImgWrapper>
     </Layout.Content>
