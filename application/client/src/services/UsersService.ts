4e15bac1eb390f8cd41f27b5a8111e5aa5e24c43
ko-he <localhost.ko@gmail.com>
2020-06-05 20:17:48 +0900

    add user params

--- application/client/src/services/UsersService.ts
+++ application/client/src/services/UsersService.ts
@@ -1,15 +1,21 @@
 import { apiClient } from './BaseService'
 class UsersService {
-  create = async() => {
+  create = async(
+    email: string,
+    username: string,
+    password: string
+  ) => {
     try {
-      const response = await apiClient.post('users')
+      const response = await apiClient.post(
+        'users',
+        { email, username, password }
+      )
       return response.data
     } catch(err) {
       console.error(err)
       throw err
     } 
   }
-
 }
 
 export default UsersService
